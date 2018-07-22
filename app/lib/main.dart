4d022adff1930231cf8c30ab412c9bc786b42c21
Fethullah Ünver <fethullah@Fethullahs-MacBook-Pro.local>
2018-07-22 19:50:34 +0200

    uppload

---
+++ app/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {     //It´s the Main
+  runApp(         //Runs the app
+    Container(      //A Container that will be filled a color
+      color: Colors.greenAccent
+    ),
+  );
+  print("TEEEEXXX");    //prits the text in the Console
+}
